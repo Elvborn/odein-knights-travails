ec88add4674c8fd2be7d0371834c197b24dec481
Elvborn <ralleejensen@gmail.com>
2024-12-19 20:23:28 +0100

    Changed search algorithm to BFS

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
-function knightMove(startCord, endCord) {
-	return recMove(startCord, endCord);
+const createNode = function (cord, parent = null) {
+	const getPrevMoves = (node = parent) => {
+		if (node === null) return [];
 
-	function recMove(current, end, prevMoves = []) {
-		prevMoves.push([current[0], current[1]]);
+		const moves = [node.cord].concat(getPrevMoves(node.parent));
+		return moves;
+	};
+
+	const getValidMoves = () => {
+		const prevMoves = getPrevMoves();
 		const validMoves = [];
 
 		// Up
-		if (current[1] + 2 <= 7) {
-			if (current[0] - 1 >= 0) addMove([current[0] - 1, current[1] + 2]);
-			if (current[0] + 1 <= 7) addMove([current[0] + 1, current[1] + 2]);
+		if (cord[1] + 2 <= 7) {
+			if (cord[0] - 1 >= 0) addMove([cord[0] - 1, cord[1] + 2]);
+			if (cord[0] + 1 <= 7) addMove([cord[0] + 1, cord[1] + 2]);
 		}
 
 		// Down
-		if (current[1] - 2 >= 0) {
-			if (current[0] - 1 >= 0) addMove([current[0] - 1, current[1] - 2]);
-			if (current[0] + 1 <= 7) addMove([current[0] + 1, current[1] - 2]);
+		if (cord[1] - 2 >= 0) {
+			if (cord[0] - 1 >= 0) addMove([cord[0] - 1, cord[1] - 2]);
+			if (cord[0] + 1 <= 7) addMove([cord[0] + 1, cord[1] - 2]);
 		}
 
 		// Left
-		if (current[0] - 2 >= 0) {
-			if (current[1] - 1 >= 0) addMove([current[0] - 2, current[1] - 1]);
-			if (current[1] + 1 <= 7) addMove([current[0] - 2, current[1] + 1]);
+		if (cord[0] - 2 >= 0) {
+			if (cord[1] - 1 >= 0) addMove([cord[0] - 2, cord[1] - 1]);
+			if (cord[1] + 1 <= 7) addMove([cord[0] - 2, cord[1] + 1]);
 		}
 
 		// Right
-		if (current[0] + 2 <= 7) {
-			if (current[1] - 1 >= 0) addMove([current[0] + 2, current[1] - 1]);
-			if (current[1] + 1 <= 7) addMove([current[0] + 2, current[1] + 1]);
+		if (cord[0] + 2 <= 7) {
+			if (cord[1] - 1 >= 0) addMove([cord[0] + 2, cord[1] - 1]);
+			if (cord[1] + 1 <= 7) addMove([cord[0] + 2, cord[1] + 1]);
 		}
 
+		// Adds a move to validMoves if position haven't been visited
 		function addMove(cord) {
 			let isValid = true;
 
 			for (let i = 0; i < prevMoves.length; i++) {
-				if (prevMoves[i][0] === cord[0] && prevMoves[i][1] === cord[1]) {
+				if (
+					prevMoves[i][0] === cord[0] &&
+					prevMoves[i][1] === cord[1]
+				) {
 					isValid = false;
 					break;
 				}
@@ -42,21 +51,101 @@ function knightMove(startCord, endCord) {
 			if (isValid) validMoves.push(cord);
 		}
 
-		for (let i = 0; i < validMoves.length; i++) {
-			if (validMoves[i][0] === end[0] && validMoves[i][1] === end[1]) {
-				prevMoves.push([end[0], end[1]]);
+		return validMoves;
+	};
 
-				return prevMoves;
+	return {
+		cord,
+		parent,
+		getValidMoves,
+		getPrevMoves,
+	};
+};
+
+function knightMove(startCord, endCord) {
+	const startNode = createNode(startCord);
+
+	let queue = [startNode];
+
+	// BFS
+	const newQueue = [];
+	const validPaths = [];
+	while (queue.length > 0) {
+		const currentNode = queue.shift();
+
+		if (
+			currentNode.cord[0] === endCord[0] &&
+			currentNode.cord[1] === endCord[1]
+		)
+			validPaths.push(
+				currentNode.getPrevMoves().reverse().concat([endCord])
+			);
+
+		currentNode.getValidMoves().forEach((move) => {
+			newQueue.push(createNode(move, currentNode));
+		});
+
+		if (queue.length === 0) {
+			if (validPaths.length > 0) {
+				return validPaths;
 			}
+
+			queue = newQueue.splice(0, newQueue.length);
+		}
+	}
+
+	return "Error!";
+
+	function getValidMoves(cord, prevMoves = []) {
+		const validMoves = [];
+
+		// Up
+		if (cord[1] + 2 <= 7) {
+			if (cord[0] - 1 >= 0) addMove([cord[0] - 1, cord[1] + 2]);
+			if (cord[0] + 1 <= 7) addMove([cord[0] + 1, cord[1] + 2]);
 		}
 
-		for (let i = 0; i < validMoves.length; i++) {
-			return recMove(validMoves[i], end, prevMoves);
+		// Down
+		if (cord[1] - 2 >= 0) {
+			if (cord[0] - 1 >= 0) addMove([cord[0] - 1, cord[1] - 2]);
+			if (cord[0] + 1 <= 7) addMove([cord[0] + 1, cord[1] - 2]);
 		}
+
+		// Left
+		if (cord[0] - 2 >= 0) {
+			if (cord[1] - 1 >= 0) addMove([cord[0] - 2, cord[1] - 1]);
+			if (cord[1] + 1 <= 7) addMove([cord[0] - 2, cord[1] + 1]);
+		}
+
+		// Right
+		if (cord[0] + 2 <= 7) {
+			if (cord[1] - 1 >= 0) addMove([cord[0] + 2, cord[1] - 1]);
+			if (cord[1] + 1 <= 7) addMove([cord[0] + 2, cord[1] + 1]);
+		}
+
+		// Adds a move to validMoves if position haven't been visited
+		function addMove(cord) {
+			let isValid = true;
+
+			for (let i = 0; i < prevMoves.length; i++) {
+				if (
+					prevMoves[i][0] === cord[0] &&
+					prevMoves[i][1] === cord[1]
+				) {
+					isValid = false;
+					break;
+				}
+			}
+
+			if (isValid) validMoves.push(cord);
+		}
+
+		return validMoves;
 	}
 }
 
-const moves = knightMove([3, 3], [0, 0]);
-console.log("Total number of moves: " + (moves.length - 1));
-console.log("Moves made:");
+const moves = knightMove([3, 3], [4, 3]);
+const moveCount = moves[0].length - 1;
+console.log(`Shortest path is ${moveCount} moves!`);
+console.log(`All possible paths:`);
 console.log(moves);
